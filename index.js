768652a690037f452a81132c1b310d52b67357f1
Mark Manley <mark.manley.2010@gmail.com>
2015-07-01 09:04:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = function () {
+
+    function emptyContentsOf (fragileArrayReference) {
+        fragileArrayReference.length = 0;
+    }
+
+    function replaceContentsOf (fragileArrayReference, newContents) {
+        emptyContentsOf(fragileArrayReference);
+        fragileArrayReference.push.apply(fragileArrayReference, newContents);
+    }
+
+    function appendTo (fragileArrayReference, moreContents) {
+        fragileArrayReference.push.apply(fragileArrayReference, moreContents);
+    }
+
+    return function (fragileArrayReference) {
+        var chain = {
+            emptyContents: function () {
+                emptyContentsOf(chain.reference);
+                return chain;
+            },
+            replaceContentsWith: function (newContents) {
+                replaceContentsOf(chain.reference, newContents);
+                return chain;
+            },
+            append: function (moreContents) {
+                appendTo(chain.reference, moreContents);
+                return chain;
+            }
+        };
+        chain.reference = fragileArrayReference;
+        return chain;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+Angbad
+
+A micro library to help you put your code in your Services, not your Controllers.
+
+ *Not Ready Yet.*
+ ---
+ Used in production, but needs to be
+ tweaked/polished before releasing it
+ into the wild.
