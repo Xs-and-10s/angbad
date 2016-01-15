264c42a946fb57861e8945ad84ea7cfd23e60f9a
Mark Manley <mark.manley.2010@gmail.com>
2016-01-15 11:02:43 -0500

    reorder functions for easier reading

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ module.exports = function () {
                 emptyContentsOf(chain.reference);
                 return chain;
             },
-            swap: function (newContents) {
-                replaceContentsOf(chain.reference, newContents);
-                return chain;
-            },
             append: function (moreContents) {
                 appendTo(chain.reference, moreContents);
+                return chain;
+            },
+            swap: function (newContents) {
+                replaceContentsOf(chain.reference, newContents);
                 return chain;
             }
         };
@@ -22,11 +22,11 @@ module.exports = function () {
     function emptyContentsOf (referenceToMutableArray) {
         referenceToMutableArray.length = 0;
     }
+    function appendTo (referenceToMutableArray, moreContents) {
+        referenceToMutableArray.push.apply(referenceToMutableArray, moreContents);
+    }
     function replaceContentsOf (referenceToMutableArray, newContents) {
         referenceToMutableArray.length = 0;
         referenceToMutableArray.push.apply(referenceToMutableArray, newContents);
-    }
-    function appendTo (referenceToMutableArray, moreContents) {
-        referenceToMutableArray.push.apply(referenceToMutableArray, moreContents);
     }
 }
